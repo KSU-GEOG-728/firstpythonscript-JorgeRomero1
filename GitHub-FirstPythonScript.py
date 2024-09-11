1af53f45a62487f384a9f179577e0dd7e3c6f56d
Jorge Romero <106196874+JorgeRomero1@users.noreply.github.com>
2024-09-11 14:23:42 -0500

    Homework
    Clip Rivers and calculate the length

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -3,9 +3,9 @@
 
 """
     File name: GitHub-FirstPythonScript.py
-    Author: ENTER YOUR  NAME
-    Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Author: Jorge Romero 
+    Description:  Calculate total river miles with expanded ecoregion boundary
+    Date created: 09/11/2024
     Python Version: 3.9.16
 """
 
@@ -14,3 +14,13 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
+arcpy.env.workspace = "C:/Users/jorgeromero/Documents/firstpythonscript/GISProject/ExerciseData.gdb"
+
+#Performe Geoprocessing 
+## selectRiver it stores the selected river "Kansas River"
+
+
+selectRiver = arcpy.SelectLayerByAttribute_management('ks_major_rivers', 'NEW_SELECTION', "GNIS_NAME = 'Kansas River'")
+selectCounties = arcpy.SelectLayerByLocation_management('Tiger2010_Census_County', 'INTERSECT', selectRiver, "", 'NEW_SELECTION')
+arcpy.CopyFeatures_management(selectCounties, 'selectedCounties')
+arcpy.management.CalculateGeometryAttributes('clippedRivers' , [['riverlenght', 'LENGTH']], 'MILES_US')
